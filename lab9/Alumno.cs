39233d79f1b7e91773eefb475dac872736c710ec
Daniel_ixcot <Daniel_ixcot@LAB4-8>
2024-04-18 09:20:44 -0600

    Agregar archivos de proyecto.

---
+++ lab9/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    public class Alumno
+    {
+        public int Dpi { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ lab9/Talleres.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    public class Talleres
+    {
+        public int Codigo { get; set; }
+        public string Taller { get; set; }
+        public int Costo { get; set; }
+    }
+}
